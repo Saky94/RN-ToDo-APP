36ddc1f12589d32fff515ef14c44f95b74a2335c
Marija Kulić <mkulic@comtrade.com>
2021-10-11 16:40:57 +0200

    My last change

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Button, FlatList, Text, Image } from 'react-native';
+import GoalInput from './components/GoalInput';
+
+import GoalItem from './components/GoalItem';
+
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const [isAddMode, setIsAddMode] = useState(false)
+
+
+
+  const addGoalHandler = goalTit => {
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value:goalTit}]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalHandler = () => {
+    setIsAddMode(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js +++ to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Text style={styles.textEdit}>PLAN YOUR DAY...</Text>
+      <Button title='Add new goal' onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalHandler} />
+
+      <FlatList 
+      keyExtractor={(item, index) => item.id}
+      data={courseGoals} 
+      renderItem={itemData => ( 
+      <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />
+      
+        
+      )} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 70
   },
+  textEdit: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  imageTT: {
+
+  }
 });
